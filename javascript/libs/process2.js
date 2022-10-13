c4254df931aa8a58ca76513a5c05089a954dcdfb
Polycarp <polycarpnwaeke@gmail.com>
2022-10-14 00:21:13 +0100

    Fixed process2 function
    Fix: #2

--- javascript/libs/process2.js
+++ javascript/libs/process2.js
@@ -1,17 +1,17 @@
 import { leadArray, ul } from "../script.js";
 
-export default function process2 (){
-    let li = document.createElement('li');
-    let a = document.createElement('a');
-    let img = document.createElement('img');
-    for (let element of leadArray) {
-      a.setAttribute('href', `${element}`);
-      a.setAttribute('target', '_blank');
-      img.setAttribute('src', './icons/icons8-delete-67.png');
-      a.textContent = element;
-      li.appendChild(a);
-      li.appendChild(img);
-      li.classList.add('flex');
-      ul.appendChild(li);
-    }
-} 
+export default function process2() {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    let img = document.createElement("img");
+    let element = leadArray[leadArray.length - 1];
+
+    a.setAttribute("href", `${element}`);
+    a.setAttribute("target", "_blank");
+    img.setAttribute("src", "./icons/icons8-delete-67.png");
+    a.textContent = element;
+    li.appendChild(a);
+    li.appendChild(img);
+    li.classList.add("flex");
+    ul.appendChild(li);
+}
